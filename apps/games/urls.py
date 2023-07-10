85df3cdae42039fa17e95554229ca83e16ed7f51
Евгений Табашнюк <@itstep.krg>
2023-07-10 20:45:37 +0600

    create render video

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 # Local
-from .views import index, about, get_game
+from .views import index, about, get_game, games
 
 
 urlpatterns = [
     path('', index),
     path('about/', about),
-    path('<int:game_id>/', get_game)
+    path('<int:game_id>/', get_game),
+    path('list/', games),
 ]
