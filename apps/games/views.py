f2936ec04a2176112e9e32b5286778454c98a72c
Евгений Табашнюк <@itstep.krg>
2023-07-17 21:12:47 +0600

    post

--- apps/games/views.py
+++ apps/games/views.py
@@ -3,30 +3,59 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.db.models.query import QuerySet
+from django.views.generic import View
 
 # Local
-from .models import Game
+from .models import Game, Genre
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    template_name: str = 'games/index.html'
-    return render(
-        request=request,
-        template_name=template_name,
-        context={}
-    )
+class MainView(View):
+    
+    def get(self, request: HttpRequest) -> HttpResponse:
+        template_name: str = 'games/index.html'
+        return render(
+            request=request,
+            template_name=template_name,
+            context={}
+        )
+    
 
-def games(request: HttpRequest) -> HttpResponse:
-    template_name: str = 'games/video.html'
-    queryset: QuerySet[Game] = Game.objects.all()
-    return render(
-        request=request,
-        template_name=template_name,
-        context={
-            'games': queryset
-        }
-    )
+class GameListView(View):
 
+    def get(self, request: HttpRequest) -> HttpResponse:
+        template_name: str = 'games/video.html'
+        queryset: QuerySet[Game] = Game.objects.all()
+        genres: QuerySet[Genre] = Genre.objects.all()
+        return render(
+            request=request,
+            template_name=template_name,
+            context={
+                'games': queryset,
+                'genres': genres
+            }
+        )
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
+        breakpoint()
+        return HttpResponse("Hello")
+
+class GameView(View):
+
+    def get(self, request: HttpRequest, game_id: int) -> HttpResponse:
+        try:
+            game: Game = Game.objects.get(id=game_id)
+        except Game.DoesNotExist as e:
+            return HttpResponse(
+                f'<h1>Игры с id {game_id} не существует!</h1>'
+            )
+        return render(
+            request=request,
+            template_name='games/store-product.html',
+            context={
+                'igor': game 
+            }
+        )
+    
 def about(request: HttpRequest) -> HttpResponse:
     template_name: str = 'games/about.html'
     return render(
@@ -35,13 +64,3 @@ def about(request: HttpRequest) -> HttpResponse:
         context={}
     )
 
-def get_game(request: HttpRequest, game_id: int) -> HttpResponse:
-    try:
-        game: Game = Game.objects.get(id=game_id)
-    except Game.DoesNotExist as e:
-        return HttpResponse(
-            f'<h1>Игры с id {game_id} не существует!</h1>'
-        )
-    return HttpResponse(
-        f'<h1>ID: {game.name}</h1>'
-    )
